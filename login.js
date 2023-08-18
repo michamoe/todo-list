c6be863eefbf4d8539dfbd74c858c01fca333a29
michamoe <98765003+michamoe@users.noreply.github.com>
2023-08-18 17:55:28 +0200

    Hallo

---
+++ login.js
@@ -0,0 +1,6 @@
+const inputCode = document.querySelector(".input-code");
+const loginButton = document.querySelector(".login-btn");
+
+if (inputCode === 2333694) {
+    Button.addEventListener('click')
+}
