0a47a853832181c2b0458481c24527879176fa3f
michamoe <98765003+michamoe@users.noreply.github.com>
2023-08-19 12:44:42 +0200

    login page almost ready

---
+++ login.js
@@ -0,0 +1,11 @@
+const inputCode = document.querySelector(".input-code");
+const loginButton = document.querySelector(".login-btn");
+const correctCode = 2333694;
+
+loginButton.addEventListener('click', function() {
+    const enteredCode = parseInt(inputCode.value.trim());
+    if (enteredCode === correctCode) {
+    } else {
+        alert("Incorrect Code");
+    }
+});
