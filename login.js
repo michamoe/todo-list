581dc1cab86d1d59fc96bb87b34139d8f1cb3fb5
michamoe <98765003+michamoe@users.noreply.github.com>
2023-08-22 17:45:19 +0200

    loginChris

--- login.js
+++ login.js
@@ -5,6 +5,7 @@ const correctCode = 2333694;
 loginButton.addEventListener('click', function() {
     const enteredCode = parseInt(inputCode.value.trim());
     if (enteredCode === correctCode) {
+        window.location = "logged_in.html"
     } else {
         alert("Incorrect Code");
     }
